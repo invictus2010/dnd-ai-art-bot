6c904fbf3bb9fedec10f8e8e2b28a4f8194f264b
Jeff Withington <jeffrey.withington@gmail.com>
2022-10-18 19:45:58 -0400

    add custom dimension

--- bot.py
+++ bot.py
@@ -47,6 +47,27 @@ async def dream(ctx, *, prompt):
                 file = discord.File(arr, filename='art.png')
                 await msg.edit(content=f"“{prompt}” \n")
                 await ctx.send(file=file)
+## This command will generate AI art for a specific dimension. I chose 738 x 251 for my purposes
+@bot.command()
+async def load(ctx, *, prompt):
+    msg = await ctx.send(f"“{prompt}”\n> Generating...")
+    answers = stability_api.generate(prompt=prompt, width=738, height= 251)
+    for resp in answers:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                warnings.warn(
+                    "Your request activated the API's safety filters and could not be processed."
+                    "Please modify the prompt and try again.")
+                msg = await ctx.send(
+                    "You have triggered the filter, please try again")
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                img = Image.open(io.BytesIO(artifact.binary))
+                arr = io.BytesIO(artifact.binary)
+                img.save(arr, format='PNG')
+                arr.seek(0)
+                file = discord.File(arr, filename='art.png')
+                await msg.edit(content=f"“{prompt}” \n")
+                await ctx.send(file=file)
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
